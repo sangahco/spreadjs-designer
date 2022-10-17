ebb6877b7cf92da1a78abc342f1a6eaded749507
Emanuele Disco <emawind84@gmail.com>
2022-10-17 13:34:00 +0900

    Update docker-auto.sh
    updated docker compose version to 1.29.2

--- docker-auto.sh
+++ docker-auto.sh
@@ -14,7 +14,7 @@ getenv(){
     echo "${_env:-$(cat .env | awk 'BEGIN { FS="="; } /^'$1'/ {sub(/\r/,"",$2); print $2;}')}"
 }
 
-DOCKER_COMPOSE_VERSION="1.14.0"
+DOCKER_COMPOSE_VERSION="1.29.2"
 CONF_ARG="-f docker-compose.yml"
 PATH=$PATH:/usr/local/bin/
 REGISTRY_URL="$(getenv REGISTRY_URL)"
